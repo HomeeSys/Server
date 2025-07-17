8c1fa046e4826ea107adcc346d55480b5338617e
DarekKrawczyk <darek9krawczyk@hotmail.com>
2025-07-17 23:08:06 +0200

    Basic `gRPC` implementation for `Devices`

---
+++ Services/Devices/Devices.GRPC/Program.cs
@@ -0,0 +1,14 @@
+using CommonServiceLibrary.Exceptions.Handlers;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGrpc();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+var app = builder.Build();
+
+app.MapGrpcService<GrpcDevicesService>();
+app.UseExceptionHandler(x => { });
+
+app.Run();
