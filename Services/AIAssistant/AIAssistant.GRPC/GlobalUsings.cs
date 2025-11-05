d99d8deb2b4e9f112c35c1f7b45024ea18778cb2
DarekKrawczyk <darek9krawczyk@hotmail.com>
2025-11-05 16:32:15 +0100

    Removed obsolete solution files

--- Services/AIAssistant/AIAssistant.GRPC/GlobalUsings.cs
+++
@@ -1,4 +0,0 @@
-﻿global using AIAssistant.GRPC.Services;
-global using AIAssistant.Implementation.General;
-global using Grpc.Core;
-
--- Services/Devices/Devices.DataAccess/Scripts/Basic.sql
+++
@@ -1,8 +0,0 @@
-﻿Select d.DeviceNumber as 'Device Number', l.Name as 'Location' from Devices as d left join Locations as l on d.LocationID = l.ID;
-
--- Select all with timestamp config and location.
-select d.DeviceNumber, d.RegisteredDate, d.Description, l.Name, tc.CRON from devices as d
-left join locations as l 
-on d.LocationID = l.ID
-left join TimestampConfigurations as tc
-on d.TimestampConfigurationID = tc.ID;
